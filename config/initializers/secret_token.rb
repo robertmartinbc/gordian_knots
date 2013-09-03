3a1b8b0cac1ad691ce3831994cf275c72161183d
Robert Martin <robertmartinbc@shaw.ca>
2013-09-02 18:53:09 -0700

    First commit and README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GordianKnots::Application.config.secret_key_base = 'f4977f713a406fadf1a7acb281e2f49ed49f42eb0e0f52ba5f70e2b6177736133badcd05ef7d296801b1d8114cf130f0b4bbf681ad83438f63289f2c275d0390'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GordianKnots::Application.config.session_store :cookie_store, key: '_gordian_knots_session'
